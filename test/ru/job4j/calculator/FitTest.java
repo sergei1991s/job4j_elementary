69ee6a14dc252735a77c14b723ad5a5f1693d29e
Sergei <s-pribylov@bk.ru>
2020-09-02 14:19:09 +0300

    6. Идеальный вес. [#276054]

--- test/ru/job4j/calculator/FitTest.java
+++ test/ru/job4j/calculator/FitTest.java
@@ -3,11 +3,12 @@ package ru.job4j.calculator;
 import org.junit.Test;
 import org.junit.Assert;
 
-import static org.junit.Assert.*;
+
+
 public class FitTest {
 
     @Test
-    public void whenman180Then92() {
+    public void whenMan180Then92() {
         short in = 180;
         double expected = 92;
         double out = Fit.manWeight(in);
@@ -15,11 +16,11 @@ public class FitTest {
     }
 
     @Test
-    public void whenwoman170Then89() {
+    public void whenWoman170Then89() {
         short in = 170;
-        double expected = 89;
-        double out = Fit.manWeight(in);
-        Assert.assertEquals(expected, out, 0.01);
+        double expected = 69;
+        double out = Fit.womanWeight(in);
+
 
     }
 }
